e97d2d3de5c107bf76f26939fee3476272eb9f57
inkink <jasonmagoo3@gmail.com>
2017-05-06 19:44:28 -0700

    Added Date Class files

---
+++ date.h
@@ -0,0 +1,32 @@
+#ifndef DATE_H
+#define DATE_H
+#include "string"
+
+class Date
+{
+public:
+    Date();
+    Date(int m, int d, int y);
+
+    int getMonth() const;
+    void setMonth(int value);
+
+    int getYear() const;
+    void setYear(int value);
+
+    int getDay() const;
+    void setDay(int value);
+
+    std::string getString() const;
+
+    void operator=(const Date& date);
+    bool operator<=(const Date& other) const;
+    bool operator>=(const Date& other) const;
+
+private:
+    int month;
+    int day;
+    int year;
+};
+
+#endif // DATE_H
