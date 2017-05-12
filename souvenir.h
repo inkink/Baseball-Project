1c41411101027570bcca6bac180c7fb5e5c01c7d
JiaweiChen110 <alph_110@hotmail.com>
2017-05-12 08:55:33 -0700

    Add files via upload

--- souvenir.h
+++ souvenir.h
@@ -6,16 +6,20 @@ using namespace std;
 class Souvenir
 {
 public:
-    Souvenir();
-    Souvenir(string name="", double cost=0,int num=0);
+//    Souvenir();
+    Souvenir(string Name="", double cost=0,int num=0);
     string getItem();
     double getPrice();
     int getQuantity();
 
+    void setItem(string n);
+    void setPrice(double p);
+    void setQuantity(int n);
+
 private:
-    string Item;
-    double Price;
-    int Quantity;
+    string item;
+    double price;
+    int quantity;
 };
 
 #endif // SOUVENIR_H
